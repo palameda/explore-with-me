b6fa29115acc97fe25fe507dc3dbb9e6b6439657
palameda <stanislav.s.mazurenko@yandex.ru>
2024-04-17 11:34:43 +0300

    feat: добавить репозиторий

--- stat-service/server/src/main/java/ru/practicum/repository/StatsDbRepository.java
+++ stat-service/server/src/main/java/ru/practicum/repository/StatsDbRepository.java
@@ -1,4 +1,26 @@
 package ru.practicum.repository;
 
-public class StatsDbRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import ru.practicum.dto.StatsDto;
+import ru.practicum.model.Event;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface StatsDbRepository extends JpaRepository<Event, Long> {
+    @Query("")
+    List<StatsDto> findAllStatistics(
+            @Param("start")LocalDateTime start,
+            @Param("end") LocalDateTime end,
+            @Param("uris") List<String> uris
+    );
+
+    @Query("")
+    List<StatsDto> findUniqueStatistics(
+            @Param("start")LocalDateTime start,
+            @Param("end") LocalDateTime end,
+            @Param("uris") List<String> uris
+    );
 }
