dab021df3f1aee4eb219f246f72e01c9b0faed80
palameda <stanislav.s.mazurenko@yandex.ru>
2024-04-17 20:56:18 +0300

    refactor: добавить скан компонентов для запуска приложения

--- stat-service/server/src/main/java/ru/practicum/App.java
+++ stat-service/server/src/main/java/ru/practicum/App.java
@@ -2,7 +2,13 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ComponentScan("application")
+@EnableJpaRepositories("practicum")
+@EntityScan("practicum")
 @SpringBootApplication
 public class App 
 {
