486887a9e9c7c11c7a73eea4bde9bfcf67551ebe
palameda <stanislav.s.mazurenko@yandex.ru>
2024-04-17 11:10:07 +0300

    feat: добавить event dto для обработки данных из тела запроса

--- stat-service/dto/src/main/java/ru/practicum/dto/EventDto.java
+++ stat-service/dto/src/main/java/ru/practicum/dto/EventDto.java
@@ -1,4 +1,25 @@
-package ru.practicum;
+package ru.practicum.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EventDto {
+    @NotBlank
+    private String app;
+    @NotBlank
+    private String uri;
+    @NotBlank
+    private String ip;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime created;
 }
