bb9c1ecdf7887734c495531178cc158e57070644
palameda <stanislav.s.mazurenko@yandex.ru>
2024-04-17 13:06:10 +0300

    refactor: добавить запрос к бд в @Query

--- stat-service/server/src/main/java/ru/practicum/repository/StatsDbRepository.java
+++ stat-service/server/src/main/java/ru/practicum/repository/StatsDbRepository.java
@@ -10,14 +10,24 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface StatsDbRepository extends JpaRepository<Event, Long> {
-    @Query("")
-    List<StatsDto> findAllStatistics(
+    @Query("SELECT new ru.practicum.dto.StatsDto(e.app, e.uri, count(e.id)) " +
+            "FROM Event as e " +
+            "WHERE (e.uri IN :uris) " +
+            "AND (e.created BETWEEN :start AND :end) " +
+            "GROUP BY e.uri, e.app " +
+            "ORDER BY count(e.id) DESC")
+    List<StatsDto> findRequiredStatistics(
             @Param("start")LocalDateTime start,
             @Param("end") LocalDateTime end,
             @Param("uris") List<String> uris
     );
 
-    @Query("")
+    @Query("SELECT new ru.practicum.dto.StatsDto(e.app, e.uri, count(DISTINCT e.id)) " +
+            "FROM Event as e " +
+            "WHERE (e.uri IN :uris) " +
+            "AND (e.created BETWEEN :start AND :end) " +
+            "GROUP BY e.uri, e.app " +
+            "ORDER BY count(e.id) DESC")
     List<StatsDto> findUniqueStatistics(
             @Param("start")LocalDateTime start,
             @Param("end") LocalDateTime end,
