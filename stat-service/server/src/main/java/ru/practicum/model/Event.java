1005c86064c79bd53b21437dda52973969dfeeb7
palameda <stanislav.s.mazurenko@yandex.ru>
2024-04-17 10:55:14 +0300

    feat: добавить в модуль server модель Event

--- stat-service/server/src/main/java/ru/practicum/model/Event.java
+++ stat-service/server/src/main/java/ru/practicum/model/Event.java
@@ -1,4 +1,22 @@
 package ru.practicum.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "statistics", schema = "public")
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String app;
+    String uri;
+    String ip;
+    @Column(name = "created")
+    LocalDateTime date;
 }
