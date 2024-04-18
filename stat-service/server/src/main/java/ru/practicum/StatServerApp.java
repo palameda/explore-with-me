a0bfd5a279d72c11f2df91973faa5eae5a1b8601
palameda <stanislav.s.mazurenko@yandex.ru>
2024-04-18 15:31:34 +0300

    удалить лишний скан компонентов

--- stat-service/server/src/main/java/ru/practicum/StatServerApp.java
+++ stat-service/server/src/main/java/ru/practicum/StatServerApp.java
@@ -1,13 +1,9 @@
-package ru;
+package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@ComponentScan("application")
-//@EnableJpaRepositories("practicum")
-@SpringBootApplication
+@SpringBootApplication()
 public class StatServerApp {
     public static void main(String[] args) {
         SpringApplication.run(StatServerApp.class, args);
