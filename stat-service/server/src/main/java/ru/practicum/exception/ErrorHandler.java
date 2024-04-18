d0cff59dc8e0d9dc50be7c4d0a2349cfe6486ae6
palameda <stanislav.s.mazurenko@yandex.ru>
2024-04-18 15:35:13 +0300

    refactor: изменить уровень логов с warn на info

--- stat-service/server/src/main/java/ru/practicum/exception/ErrorHandler.java
+++ stat-service/server/src/main/java/ru/practicum/exception/ErrorHandler.java
@@ -7,27 +7,27 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-@RestControllerAdvice
 @Slf4j
+@RestControllerAdvice
 public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleBadRequest(final MethodArgumentTypeMismatchException exception) {
-        log.warn("Произошла ошибка: {}", exception.getMessage());
+        log.info("Произошла ошибка: {}", exception.getMessage());
         return new ErrorResponse(exception.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleException(final BadRequestException exception) {
-        log.warn("Произошла ошибка: {}", exception.getMessage());
+        log.info("Произошла ошибка: {}", exception.getMessage());
         return new ErrorResponse(exception.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleException(final Exception exception) {
-        log.warn("Произошла ошибка: {}", exception.getMessage());
+        log.info("Произошла ошибка: {}", exception.getMessage());
         return new ErrorResponse(exception.getMessage());
     }
 }
