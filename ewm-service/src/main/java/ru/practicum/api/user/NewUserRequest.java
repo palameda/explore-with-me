02fb580962fa30f809ce8d9cbfa808a9e91e7dab
palameda <stanislav.s.mazurenko@yandex.ru>
2024-04-23 14:05:50 +0300

    feat: добавить dto классы для сущности User

---
+++ ewm-service/src/main/java/ru/practicum/api/user/NewUserRequest.java
@@ -0,0 +1,18 @@
+package ru.practicum.api.user;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+public class NewUserRequest {
+    @Size(min = 6, max = 254, message = "Поле email должно быть не короче 6 и не длиннее 254 символов")
+    @Email
+    @NotBlank
+    private String email;
+    @Size(min = 2, max = 250, message = "Поле name должно быть не короче 2 и не длиннее 250 символов")
+    @NotBlank
+    private String name;
+}
---
+++ ewm-service/src/main/java/ru/practicum/api/user/UserShortDto.java
@@ -0,0 +1,9 @@
+package ru.practicum.api.user;
+
+import lombok.Data;
+
+@Data
+public class UserShortDto {
+    private String email;
+    private String name;
+}
