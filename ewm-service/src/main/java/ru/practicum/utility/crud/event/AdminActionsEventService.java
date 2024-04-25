103ae6c37d9199ace1861bb47e0b363090126435
palameda <stanislav.s.mazurenko@yandex.ru>
2024-04-25 14:23:22 +0300

    feat: добавить интерфейс сервиса, обрабатывающий действия администратора по отношению к событиям

--- ewm-service/src/main/java/ru/practicum/utility/crud/event/AdminActionsEventService.java
+++ ewm-service/src/main/java/ru/practicum/utility/crud/event/AdminActionsEventService.java
@@ -1,4 +1,18 @@
 package ru.practicum.utility.crud.event;
 
-public interface AdminActionsEventService {
+import ru.practicum.model.State;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface AdminActionsEventService<D, ID, UID, CID, R> {
+    D confirmEventByAdmin(ID eventId, R request);
+
+    List<D> getAllEventsForAdmins(List<UID> usersIds,
+                                  List<State> states,
+                                  List<CID> categoryIds,
+                                  LocalDateTime start,
+                                  LocalDateTime end,
+                                  Integer from,
+                                  Integer size);
 }
