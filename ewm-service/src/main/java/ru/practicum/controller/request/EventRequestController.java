ddf676aba2d32325b9074eb915489b52caf5effb
palameda <stanislav.s.mazurenko@yandex.ru>
2024-04-26 16:11:28 +0300

    feat: добавить контроллер для Request

--- ewm-service/src/main/java/ru/practicum/controller/request/EventRequestController.java
+++ ewm-service/src/main/java/ru/practicum/controller/request/EventRequestController.java
@@ -1,2 +1,59 @@
-package ru.practicum.controller.request;public class EventRequestController {
+package ru.practicum.controller.request;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.controller.logs.ControllerLog;
+import ru.practicum.dto.request.EventRequestStatusUpdateRequest;
+import ru.practicum.dto.request.EventRequestStatusUpdateResult;
+import ru.practicum.dto.request.ParticipationRequestDto;
+import ru.practicum.service.request.RequestService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/users")
+@RequiredArgsConstructor
+public class EventRequestController {
+    private final RequestService requestService;
+
+    @ControllerLog
+    @PostMapping("/{userId}/requests")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ParticipationRequestDto createRequest(@PathVariable("userId") Long userId,
+                                                 @RequestParam("eventId") Long eventId) {
+        return requestService.createRequest(userId, eventId);
+    }
+
+    @ControllerLog
+    @GetMapping("/{userId}/requests")
+    @ResponseStatus(HttpStatus.OK)
+    public List<ParticipationRequestDto> getUserRequest(@PathVariable("userId") Long userId) {
+        return requestService.getUserRequests(userId);
+    }
+
+    @ControllerLog
+    @GetMapping("/{userId}/events/{eventId}/requests")
+    @ResponseStatus(HttpStatus.OK)
+    public List<ParticipationRequestDto> getRequestsByEvent(@PathVariable("userId") Long userId,
+                                                            @PathVariable("eventId") Long eventId) {
+        return requestService.getRequestsToUserEvent(userId, eventId);
+    }
+
+    @ControllerLog
+    @PatchMapping("/{userId}/events/{eventId}/requests")
+    @ResponseStatus(HttpStatus.OK)
+    public EventRequestStatusUpdateResult confirmRequestsByEvent(@PathVariable("userId") Long userId,
+                                                                 @PathVariable("eventId") Long eventId,
+                                                                 @RequestBody EventRequestStatusUpdateRequest updateRequest) {
+        return requestService.confirmRequests(userId, eventId, updateRequest);
+    }
+
+    @ControllerLog
+    @PatchMapping("/{userId}/requests/{requestId}/cancel")
+    @ResponseStatus(HttpStatus.OK)
+    public ParticipationRequestDto cancelRequest(@PathVariable("userId") Long userId,
+                                                 @PathVariable("requestId") Long requestId) {
+        return requestService.cancelRequest(userId, requestId);
+    }
 }
