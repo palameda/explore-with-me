51d6d47cf3f0083d28ba3ddc86c6d3a96bf7c75a
palameda <stanislav.s.mazurenko@yandex.ru>
2024-04-26 16:10:52 +0300

    feat: добавить контроллеры для Compilation

--- ewm-service/src/main/java/ru/practicum/controller/compilation/CompilationAdminController.java
+++ ewm-service/src/main/java/ru/practicum/controller/compilation/CompilationAdminController.java
@@ -1,4 +1,41 @@
 package ru.practicum.controller.compilation;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.controller.logs.ControllerLog;
+import ru.practicum.dto.compilation.CompilationDto;
+import ru.practicum.dto.compilation.NewCompilationDto;
+import ru.practicum.dto.compilation.UpdateCompilationRequest;
+import ru.practicum.service.compilation.CompilationService;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping(path = "/admin/compilations")
+@RequiredArgsConstructor
 public class CompilationAdminController {
+    private final CompilationService compilationService;
+
+    @ControllerLog
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CompilationDto addCompilation(@RequestBody @Valid NewCompilationDto newCompilationDto) {
+        return compilationService.create(newCompilationDto);
+    }
+
+    @ControllerLog
+    @PatchMapping("/{compilationId}")
+    @ResponseStatus(HttpStatus.OK)
+    public CompilationDto updateCompilation(@RequestBody @Valid UpdateCompilationRequest updateCompilationRequest,
+                                            @PathVariable("compilationId") Long id) {
+        return compilationService.update(id, updateCompilationRequest);
+    }
+
+    @ControllerLog
+    @DeleteMapping("/{compilationId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCompilation(@PathVariable("compilationId") Long id) {
+        compilationService.delete(id);
+    }
 }
--- ewm-service/src/main/java/ru/practicum/controller/compilation/CompilationController.java
+++ ewm-service/src/main/java/ru/practicum/controller/compilation/CompilationController.java
@@ -1,4 +1,30 @@
 package ru.practicum.controller.compilation;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.controller.logs.ControllerLog;
+import ru.practicum.dto.compilation.CompilationDto;
+import ru.practicum.service.compilation.CompilationService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/compilations")
+@RequiredArgsConstructor
 public class CompilationController {
+    private final CompilationService compilationService;
+
+    @ControllerLog
+    @GetMapping
+    public List<CompilationDto> getCompilations(@RequestParam(value = "pinned", defaultValue = "false") Boolean pinned,
+                                                @RequestParam(value = "from", defaultValue = "0") Integer from,
+                                                @RequestParam(value = "size", defaultValue = "10") Integer size) {
+        return List.copyOf(compilationService.getCompilationsByPinned(pinned, from, size));
+    }
+
+    @ControllerLog
+    @GetMapping("/{compId}")
+    public CompilationDto getCompilation(@PathVariable("compId") Long compId) {
+        return compilationService.get(compId);
+    }
 }
