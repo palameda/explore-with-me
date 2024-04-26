2a1e061345365b10f95686407160672237140431
palameda <stanislav.s.mazurenko@yandex.ru>
2024-04-26 12:55:16 +0300

    feat: добавить сервис и его реализацию для Event

--- ewm-service/src/main/java/ru/practicum/service/event/EventService.java
+++ ewm-service/src/main/java/ru/practicum/service/event/EventService.java
@@ -1,16 +1,32 @@
-package ru.practicum.service;
+package ru.practicum.service.event;
 
 import ru.practicum.api.event.*;
+import ru.practicum.model.EventSort;
 import ru.practicum.utility.crud.DefaultGetService;
 import ru.practicum.utility.crud.event.AdminActionsEventService;
 import ru.practicum.utility.crud.user.UserRequestCreateService;
+import ru.practicum.utility.crud.user.UserRequestGetAllService;
 import ru.practicum.utility.crud.user.UserRequestGetService;
 import ru.practicum.utility.crud.user.UserRequestUpdateService;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface EventService extends
         DefaultGetService<Long, EventFullDto>,
         UserRequestCreateService<EventFullDto, Long, NewEventDto>,
-        UserRequestGetService<EventShortDto, Long, Long>,
+        UserRequestGetAllService<EventShortDto, Long>,
+        UserRequestGetService<EventFullDto, Long, Long>,
         UserRequestUpdateService<EventFullDto, Long, Long, UpdateEventUserRequest>,
         AdminActionsEventService<EventFullDto, Long, Long, Long, UpdateEventAdminRequest> {
+
+    List<EventShortDto> getPublicEvents(String text,
+                                        List<Long> categories,
+                                        Boolean paid,
+                                        LocalDateTime start,
+                                        LocalDateTime end,
+                                        Boolean isAvailable,
+                                        EventSort eventSort,
+                                        Integer from,
+                                        Integer size);
 }
