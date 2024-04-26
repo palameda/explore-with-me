779a76e8d6eac8767a5d15c49c6dea3fea9a4505
palameda <stanislav.s.mazurenko@yandex.ru>
2024-04-26 12:50:51 +0300

    refactor: вынести сигнатуру метода getAllByUser в отдельный интерфейс

--- ewm-service/src/main/java/ru/practicum/utility/crud/user/UserRequestGetService.java
+++ ewm-service/src/main/java/ru/practicum/utility/crud/user/UserRequestGetService.java
@@ -3,7 +3,6 @@ package ru.practicum.utility.crud.user;
 import java.util.Collection;
 
 public interface UserRequestGetService<D, ID, EID> {
-    Collection<D> getAllByUser(ID id, Integer from, Integer size);
 
     D get(ID userId, EID id);
 }
