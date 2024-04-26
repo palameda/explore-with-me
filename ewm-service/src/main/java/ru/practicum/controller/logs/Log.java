3c26e78b806627f1a0d14ec316f6dfcad7530bc2
palameda <stanislav.s.mazurenko@yandex.ru>
2024-04-26 16:07:35 +0300

    feat: добавить custom аннотацию для логирования в контроллерах

--- ewm-service/src/main/java/ru/practicum/controller/logs/Log.java
+++ ewm-service/src/main/java/ru/practicum/controller/logs/Log.java
@@ -1,4 +1,29 @@
 package ru.practicum.controller.logs;
 
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Aspect
+@Component
+@RequiredArgsConstructor
 public class Log {
+    private final HttpServletRequest httpRequest;
+
+    @Pointcut("@annotation(ControllerLog)")
+    void loggingProcess() {
+    }
+
+    @Before("loggingProcess()")
+    @SneakyThrows
+    void doLogging() {
+        log.info("Метод: {}, URI: {}, Запрос: {}", httpRequest.getMethod(), httpRequest.getRequestURI(), httpRequest.getQueryString());
+    }
 }
