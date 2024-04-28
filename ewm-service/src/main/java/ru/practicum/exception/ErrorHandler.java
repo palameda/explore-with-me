882fb7c0f97851cbdb5c238642ad217d8cba40f0
palameda <stanislav.s.mazurenko@yandex.ru>
2024-04-28 12:00:25 +0300

    refactor: добавить Aspect для логирования исключений в ErrorHandler с помощью custom аннотации

--- ewm-service/src/main/java/ru/practicum/exception/ErrorHandler.java
+++ ewm-service/src/main/java/ru/practicum/exception/ErrorHandler.java
@@ -1,46 +1,57 @@
 package ru.practicum.exception;
 
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.practicum.dto.exception.ApiError;
+import ru.practicum.exception.logs.ExceptionLog;
 
+@Slf4j
+@ToString
 @RestControllerAdvice
 public class ErrorHandler {
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler
-    public ApiError handleBadDateRangeException(final DataIntegrityViolationException e) {
+    @ExceptionLog
+    public ApiError handleDateParameterException(final DataIntegrityViolationException e) {
         return ApiError.createApiErrorFromException(e, HttpStatus.CONFLICT);
     }
 
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler
-    public ApiError handleDeleteCategoryException(final DeleteEntityException e) {
+    @ExceptionLog
+    public ApiError handleDeleteEntityException(final DeleteEntityException e) {
         return ApiError.createApiErrorFromException(e, HttpStatus.CONFLICT);
     }
 
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler
-    public ApiError handleForbiddenToChangeException(final ForbiddenActionException e) {
+    @ExceptionLog
+    public ApiError handleForbiddenActionException(final ForbiddenActionException e) {
         return ApiError.createApiErrorFromException(e, HttpStatus.CONFLICT);
     }
 
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler
+    @ExceptionLog
     public ApiError handleRequestProcessingException(final RequestProcessingException e) {
         return ApiError.createApiErrorFromException(e, HttpStatus.CONFLICT);
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler
-    public ApiError handleBadDateRangeException(final DateParameterException e) {
+    @ExceptionLog
+    public ApiError handleDateParameterException(final DateParameterException e) {
         return ApiError.createApiErrorFromException(e, HttpStatus.BAD_REQUEST);
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler
+    @ExceptionLog
     public ApiError handleNotFoundException(final NotFoundException e) {
         return ApiError.createApiErrorFromException(e, HttpStatus.NOT_FOUND);
     }
