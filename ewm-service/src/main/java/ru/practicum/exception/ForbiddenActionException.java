608669f01dd81d65d10de498e71735ab7a0cb3e9
palameda <stanislav.s.mazurenko@yandex.ru>
2024-05-02 13:56:53 +0300

    refactor: изменить ForbiddenActionException на DataConflictException для лучшего соответствия HttpStatus

--- ewm-service/src/main/java/ru/practicum/exception/ForbiddenActionException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.exception;
-
-public class ForbiddenActionException extends RuntimeException {
-    public ForbiddenActionException(String message) {
-        super(message);
-    }
-}
